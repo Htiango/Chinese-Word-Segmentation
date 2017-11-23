7bf89e929ef9f62b22c446f542711be9b118beae
Htiango <hongty106@gmail.com>
2017-11-23 17:50:44 -0500

    remove the last \n from each line

--- main/main.py
+++ main/main.py
@@ -2,31 +2,47 @@
 
 def get_seg(path):
     lines = read_convert(path)
-    corpus = generate_dict(lines)
+    corpus_dict, index_dict = generate_dict(lines)
+    for line in lines:
+        space_index = get_space_index(line)
 
+
+
+def get_space_index(s):
+    ls = s.split("  ")
+    result = [-1]
+    for word in ls:
+        index = result[-1] + len(word)
+        result.append(index)
+    result.pop(0)
+    return result
 
 
 def generate_dict(lines):
     """
     generate the whole corpus
     :param lines: input lines
-    :return: the corpus, a dict, key is the 2-gram or 1-gram word, value is its index from 0-n
+    :return: 2 dicts: one whose key is the 2-gram or 1-gram word, value is its index from 0-n,
+            the other whose ksy is the index and value is the word
     """
-    corpus = {}
+    corpus_dict = {}
+    index_dict = {}
     corpus_index = 0
     for line in lines:
         line_consecutive = line.replace(" ", "")
         for i in range(len(line_consecutive)):
             character = line_consecutive[i]
             word = line_consecutive[i:i + 2]
-            if character not in corpus:
-                corpus[character] = corpus_index
+            if character not in corpus_dict:
+                corpus_dict[character] = corpus_index
+                index_dict[corpus_index] = character
                 corpus_index += 1
-            if word not in corpus:
-                corpus[word] = corpus_index
+            if word not in corpus_dict:
+                corpus_dict[word] = corpus_index
+                index_dict[corpus_index] = word
                 corpus_index += 1
-    print(len(corpus))
-    return corpus
+    print(len(corpus_dict))
+    return corpus_dict, index_dict
 
 
 
@@ -40,6 +56,7 @@ def read_convert(path):
     lines = []
     with open(path, encoding=encoding, errors='ignore') as f:
         for line in f:
+            line = line.strip('\n')
             lines.append(line)
     print(len(lines))
     return lines

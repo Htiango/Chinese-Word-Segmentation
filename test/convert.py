a63e28812aa1c903bca784e33d6e46a3b01dc4c0
Htiango <hongty106@gmail.com>
2017-11-24 11:58:59 -0500

    modify the convert testing

--- test/convert.py
+++ test/convert.py
@@ -1,16 +1,21 @@
+"""
+Test reading and converting.
+"""
 
-
-encoding = 'big5hkscs'
-lines = []
-num_errors = 0
-with open('training.txt', encoding=encoding, errors='ignore') as f:
-    while 1:
-        try:
-            line = f.readline()
-            if line == "":
-                break
+def convert(path):
+    """
+    In this convert function, I just ignore the UnicodeDecodeError
+    :param path: the path of the input file
+    :return:
+    """
+    encoding = 'big5hkscs'
+    lines = []
+    with open(path, encoding=encoding, errors='ignore') as f:
+        for line in f:
             lines.append(line)
-        except UnicodeDecodeError as e:
-            num_errors += 1
-print('Encountered %d decoding errors.' % num_errors)
-print(len(lines))
+    print(len(lines))
+
+
+if __name__ == '__main__':
+    path = '../data/training.txt'
+    convert(path)
